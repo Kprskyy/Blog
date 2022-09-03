6cf44ae00b49468d40b5333c5536e3345141471b
Kprskyy <Kprskyyy@gmail.com>
2022-09-03 14:57:51 +0200

    Main

--- public/app.js
+++ public/app.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-console.log("Cześć")
-=======
 
->>>>>>> 8dc9c67ec575ff81cdea730d329350c0afa02255
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express')
 const app = express()
 const port = 3000
@@ -8,14 +7,3 @@ app.use(express.static('public'))
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-=======
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.use(express.static('public'))
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
->>>>>>> 8dc9c67ec575ff81cdea730d329350c0afa02255
